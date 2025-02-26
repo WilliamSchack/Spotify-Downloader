c62e1278f227f7289afbff81e4a50fc75896d62a
William Schack <54973797+WilliamSchack@users.noreply.github.com>
2025-02-26 20:26:35 +1100

    Forgot to add version
    - Updated version to v1.6.6

--- Spotify Downloader/Network/VersionManager.h
+++ Spotify Downloader/Network/VersionManager.h
@@ -11,7 +11,7 @@
 
 class VersionManager {
 	public:
-		static inline const QString VERSION = "1.6.5";
+		static inline const QString VERSION = "1.6.6";
 	public:
 		static bool UpdateAvailable();
 		static QString LatestVersion();
