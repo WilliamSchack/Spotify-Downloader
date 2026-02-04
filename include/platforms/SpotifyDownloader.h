f08fedddc25a742ff62694513f1e90cd1561e93b
William Schack <wil.schack@gmail.com>
2026-02-04 22:35:56 +1100

    Implemented SpotifyDownloader interface properly

--- include/platforms/SpotifyDownloader.h
+++ include/platforms/SpotifyDownloader.h
@@ -9,6 +9,9 @@ class SpotifyDownloader : public IPlatformDownloader
         ELinkType GetLinkType(const std::string& url) override;
 
         bool DownloadTrack(const std::string& url, const std::string& directory) override;
+        bool DownloadPlaylist(const std::string& url, const std::string& directory) override { return false; }
+        bool DownloadAlbum(const std::string& url, const std::string& directory)    override { return false; }
+        bool DownloadEpisode(const std::string& url, const std::string& directory)  override { return false; }
 };
 
 #endif
