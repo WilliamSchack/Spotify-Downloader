8420c84e06ba9bdb7dc7f086d53890949bdb5168
William Schack <wil.schack@gmail.com>
2026-02-05 22:32:00 +1100

    Added more functions to StringUtils

--- include/utilities/StringUtils.h
+++ include/utilities/StringUtils.h
@@ -3,13 +3,17 @@
 
 #include <string>
 #include <cctype>
+#include <vector>
 #include <algorithm>
 
 class StringUtils
 {
     public:
+        static bool StartsWith(const std::string& string, const std::string& value);
         static bool Contains(const std::string& string, const std::string& value);
         static void ToLower(std::string& string);
+        static std::string RemoveLast(const std::string& string);
+        static std::vector<std::string> Split(const std::string& string, const std::string& delimiter);
 };
 
 #endif
--- src/utilities/StringUtils.cpp
+++ src/utilities/StringUtils.cpp
@@ -1,5 +1,10 @@
 #include "StringUtils.h"
 
+bool StringUtils::StartsWith(const std::string& string, const std::string& value)
+{
+    return string.rfind(value, 0) == 0;
+}
+
 bool StringUtils::Contains(const std::string& string, const std::string& value)
 {
     return string.find(value) != std::string::npos;
@@ -9,4 +14,25 @@ void StringUtils::ToLower(std::string& string)
 {
     std::transform(string.begin(), string.end(), string.begin(),
         [](unsigned char c){ return std::tolower(c); });
+}
+
+std::string StringUtils::RemoveLast(const std::string& string)
+{
+    return string.substr(0, string.size() - 1);
+}
+
+std::vector<std::string> StringUtils::Split(const std::string& string, const std::string& delimiter)
+{
+    std::vector<std::string> tokens;
+    size_t start = 0;
+    size_t end = string.find(delimiter);
+    
+    while (end != std::string::npos) {
+        tokens.push_back(string.substr(start, end - start));
+        start = end + 1;
+        end = string.find(delimiter, start);
+    }
+    
+    tokens.push_back(string.substr(start));
+    return tokens;
 }
