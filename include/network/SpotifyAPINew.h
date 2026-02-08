4e7ca52df681fc295adb4bf1a91a7e262680c6a5
William Schack <wil.schack@gmail.com>
2026-02-08 14:51:53 +1100

    Testing lexbor, got track title
    This is a bit of work to use but im gonna make a wrapper for it and see if i can get it working sequentially without the callback

--- include/network/SpotifyAPINew.h
+++ include/network/SpotifyAPINew.h
@@ -1,4 +1,19 @@
+// Translation from https://github.com/AliAkhtari78/SpotifyScraper
+// Only contains things relevant to this project
+
 #ifndef SPOTIFYAPINEW_H
 #define SPOTIFYAPINEW_H
+
+#include "NetworkRequest.h"
+#include "TrackData.h"
+#include "JsonUtils.h"
+
+#include <regex>
+
+class SpotifyAPINew
+{
+    public:
+        static TrackData GetTrack(const std::string& id);
+};
 
 #endif
