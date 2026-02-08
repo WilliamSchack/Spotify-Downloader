3ac5ccef836cc1161427a2e600f71f8bff1bdca9
William Schack <wil.schack@gmail.com>
2026-02-08 15:22:27 +1100

    Setting up objects in HtmlParser

--- include/html/HtmlParser.h
+++ include/html/HtmlParser.h
@@ -1,9 +1,28 @@
 #ifndef HTMLPARSER_H
 #define HTMLPARSER_H
 
+#include <lexbor/html/parser.h>
+#include <lexbor/css/css.h>
+#include <lexbor/selectors/selectors.h>
+
+#include <string>
+#include <iostream>
+
 class HtmlParser
 {
+    public:
+        HtmlParser(const std::string& html);
+        ~HtmlParser();
 
+        lxb_status_t GetStatus();
+    private:
+        lxb_html_document* _document;
+        lxb_css_parser_t* _parser;
+        lxb_selectors_t* _selectors;
+
+        lxb_status_t _lastStatus;
+    private:
+        void CheckLastStatus();
 };
 
 #endif
--- src/html/HtmlParser.cpp
+++ src/html/HtmlParser.cpp
@@ -1,1 +1,32 @@
 #include "HtmlParser.h"
+
+HtmlParser::HtmlParser(const std::string& html)
+{
+    // Setup document
+    const lxb_char_t* lxbHtml = reinterpret_cast<const lxb_char_t*>(html.c_str());
+    _document = lxb_html_document_create();
+    _lastStatus = lxb_html_document_parse(_document, lxbHtml, html.size());
+    if (_lastStatus != LXB_STATUS_OK) return;
+
+    // Create parser
+    _parser = lxb_css_parser_create();
+    _lastStatus = lxb_css_parser_init(_parser, NULL);
+    if (_lastStatus != LXB_STATUS_OK) return;
+
+    // Create selectors
+    _selectors = lxb_selectors_create();
+    _lastStatus = lxb_selectors_init(_selectors);
+    if (_lastStatus != LXB_STATUS_OK) return;
+}
+
+HtmlParser::~HtmlParser()
+{
+    lxb_html_document_destroy(_document);
+    lxb_css_parser_destroy(_parser, true);
+    lxb_selectors_destroy(_selectors, true);
+}
+
+lxb_status_t HtmlParser::GetStatus()
+{
+    return _lastStatus;
+}
