1a2f5daf40c3c9f80a587c519acf498a12f61413
William Schack <wil.schack@gmail.com>
2026-02-10 20:13:36 +1100

    Fixed moving Image
    Now doing image = otherImage moves the image

--- include/images/Image.h
+++ include/images/Image.h
@@ -13,21 +13,37 @@ struct Image
     Image() : Data{} {}
 
     ~Image() {
-        if (Data)
-            stbi_image_free(Data);
-    }
-
-    // Move constructor
-    Image(Image&& other)
-        : Width(other.Width), Height(other.Height), Channels(other.Channels), Data(other.Data)
-    {
-        other.Data = nullptr;
+        if (Data) stbi_image_free(Data);
     }
 
     // Non-copyable, causes double free if copied
     Image (const Image&) = delete;
     Image& operator= (const Image&) = delete;
 
+    // Handle moving
+    Image(Image&& other)
+        : Width(other.Width), Height(other.Height), Channels(other.Channels), Data(other.Data)
+    {
+        other.Data = nullptr;
+    }
+
+    Image& operator=(Image&& other)
+    {
+        if (this != &other) {
+            if (Data) stbi_image_free(Data);
+            Width = other.Width;
+            Height = other.Height;
+            Channels = other.Channels;
+            Data = other.Data;
+            
+            other.Data = nullptr;
+            other.Width = 0;
+            other.Height = 0;
+            other.Channels = 0;
+        }
+
+        return *this;
+    }
 };
 
 #endif
