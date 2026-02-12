b7be94ee2579f13454a53f863373dccee6c73f66
William Schack <wil.schack@gmail.com>
2026-02-12 13:00:30 +1100

    Getting version in ytdlp
    Just testing processes, ill move this to another file

--- include/dependencies/Ytdlp.h
+++ include/dependencies/Ytdlp.h
@@ -1,9 +1,14 @@
 #ifndef YTDLP_H
 #define YTDLP_H
 
+#include "FileUtils.h"
+
+#include <string>
+
 class Ytdlp
 {
-    
+    public:
+        static std::string GetVersion();
 };
 
 #endif
--- src/dependencies/Ytdlp.cpp
+++ src/dependencies/Ytdlp.cpp
@@ -1,1 +1,30 @@
 #include "Ytdlp.h"
+
+std::string Ytdlp::GetVersion()
+{
+    std::filesystem::path executablePath = FileUtils::GetExecutablePath();
+    std::filesystem::path ytdlpPath = executablePath.parent_path() / YTDLP_PATH_RELATIVE;
+
+    std::string testCommand = ytdlpPath.string() + " --version";
+
+    FILE* pipe = popen(testCommand.c_str(), "r");
+    if (pipe == NULL) {
+        perror("Failed to open ytdlp");
+        return "";
+    }
+
+    std::string output = "";
+    char buffer[256];
+    
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+        output += buffer;
+    }
+    
+    int exitStatus = pclose(pipe);
+    if (exitStatus == -1) {
+        perror("Failed to close ytdlp");
+        return "";
+    }
+
+    return output;
+}
