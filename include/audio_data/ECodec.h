8b6a25cb99f1ea77cc4d2bd61919a2694fbaf5d0
William Schack <wil.schack@gmail.com>
2026-02-13 11:44:28 +1100

    Added ECodec and Config (For testing atm)

---
+++ include/audio_data/ECodec.h
@@ -0,0 +1,14 @@
+#ifndef ECODEC_H
+#define ECODEC_H
+
+enum class ECodec
+{
+    M4A,
+    AAC,
+    MP3,
+    OGG,
+    WAV,
+    FLAC
+};
+
+#endif
---
+++ include/config/Config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "ECodec.h"
+
+class Config
+{
+    public:
+        static inline ECodec Codec = ECodec::MP3;
+};
+
+#endif
