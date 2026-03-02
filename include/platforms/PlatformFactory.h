a0318cb1509cb8fe848d91b90f016b9e5f8230fc
William Schack <wil.schack@gmail.com>
2026-03-02 17:58:46 +1100

    Added CreateSearcher to PlatformFactory

--- include/platforms/PlatformFactory.h
+++ include/platforms/PlatformFactory.h
@@ -3,13 +3,15 @@
 
 #include "EPlatform.h"
 #include "SpotifyDownloader.h"
+#include "YoutubeSearcher.h"
 
 #include <memory>
 
 class PlatformFactory
 {
     public:
-        static std::unique_ptr<IPlatformDownloader> Create(const EPlatform& platform);
+        static std::unique_ptr<IPlatformDownloader> CreateDownloader(const EPlatform& platform);
+        static std::unique_ptr<IPlatformSearcher> CreateSearcher(const EPlatform& platform);
 };
 
 #endif
--- src/platforms/PlatformFactory.cpp
+++ src/platforms/PlatformFactory.cpp
@@ -1,9 +1,18 @@
 #include "PlatformFactory.h"
 
-std::unique_ptr<IPlatformDownloader> PlatformFactory::Create(const EPlatform& platform)
+std::unique_ptr<IPlatformDownloader> PlatformFactory::CreateDownloader(const EPlatform& platform)
 {
     switch (platform) {
         case EPlatform::Spotify: return std::make_unique<SpotifyDownloader>();
+    }
+
+    return nullptr;
+}
+
+std::unique_ptr<IPlatformSearcher> PlatformFactory::CreateSearcher(const EPlatform& platform)
+{
+    switch (platform) {
+        case EPlatform::YouTube: return std::make_unique<YoutubeSearcher>();
     }
 
     return nullptr;
