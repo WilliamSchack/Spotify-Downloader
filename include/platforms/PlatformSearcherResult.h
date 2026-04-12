6bf4c96aeedea5b23faa736fd3a10325f5f5ca92
William Schack <wil.schack@gmail.com>
2026-04-12 19:40:14 +1000

    Added print to platformsearcherresult

--- include/platforms/PlatformSearcherResult.h
+++ include/platforms/PlatformSearcherResult.h
@@ -9,6 +9,21 @@ struct PlatformSearcherResult
     double Confidence = 0.0;
 
     PlatformSearcherResult() : Data(EPlatform::Unknown) {}
+    void Print() const;
 };
+
+inline void to_json(nlohmann::json& json, const PlatformSearcherResult& data)
+{
+    json = {
+        {"confidence", data.Confidence},
+        {"data", data.Data}
+    };
+}
+
+inline void PlatformSearcherResult::Print() const
+{
+    nlohmann::json json = *this;
+    std::cout << json.dump(4) << std::endl;
+}
 
 #endif
