5465127edd935d6f2324ffcd9ea010fb2b01dbbe
William Schack <wil.schack@gmail.com>
2026-04-12 17:18:01 +1000

    Updated print in AlbumTracks

--- include/audio_data/AlbumTracks.h
+++ include/audio_data/AlbumTracks.h
@@ -12,6 +12,21 @@ struct AlbumTracks
     std::vector<TrackData> Tracks;
 
     AlbumTracks() : Data(EPlatform::Unknown) {}
+    void Print() const;
 };
+
+inline void to_json(nlohmann::json& json, const AlbumTracks& data)
+{
+    json = {
+        {"data", data.Data},
+        {"tracks", data.Tracks}
+    };
+}
+
+inline void AlbumTracks::Print() const
+{
+    nlohmann::json json = *this;
+    std::cout << json.dump(4) << std::endl;
+}
 
 #endif
