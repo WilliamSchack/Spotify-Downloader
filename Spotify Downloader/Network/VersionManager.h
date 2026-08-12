d7cc83d3e25b1742469466d30ae66149758bac91
William Schack <wil.schack@gmail.com>
2026-08-12 12:58:34 +1000

    Updated version to 1.11.7

--- Spotify Downloader/Network/VersionManager.h
+++ Spotify Downloader/Network/VersionManager.h
@@ -11,7 +11,7 @@
 
 class VersionManager {
 	public:
-		static inline const QString VERSION = "1.11.6";
+		static inline const QString VERSION = "1.11.7";
 	public:
 		static bool UpdateAvailable();
 		static QString LatestVersion();
